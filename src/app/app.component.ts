30b6488c61e19b28dc27f1fc4ec9471b7740e3bd
Daniel Garcia Uscanga <dalokdaga@gmail.com>
2021-04-08 19:52:19 -0500

    hasta video 10 pipe Async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Capitán América';
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI: number = Math.PI;
+  porcentaje:number =0.234;
+  salario:number = 1234.5;
+
+  valorPromesa = new Promise<string>((resolve) =>{
+    setTimeout((() => {
+      resolve('llego la data');
+    }),4500);
+  });
+
+  heroe ={
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: '500',    
+    direccion: {
+      calle: 'Prime',
+      casa: 20
+    }
+  }
 }
