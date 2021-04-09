1841d9735ff108e6efc548ff13eca4dd90ac0cd5
Daniel Garcia Uscanga <dalokdaga@gmail.com>
2021-04-08 22:15:55 -0500

    hasta video 12 registro de idiomas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,13 @@ export class AppComponent {
   PI: number = Math.PI;
   porcentaje:number =0.234;
   salario:number = 1234.5;
-
+  fecha : Date = new Date();
   valorPromesa = new Promise<string>((resolve) =>{
     setTimeout((() => {
       resolve('llego la data');
     }),4500);
   });
-
+  idioma = 'fr';
   heroe ={
     nombre: 'Logan',
     clave: 'Wolverine',
@@ -27,4 +27,15 @@ export class AppComponent {
       casa: 20
     }
   }
+
+  btnEspaniol(){
+    this.idioma = 'es';
+  }
+  btnIngles(){
+    this.idioma = 'en';
+  }
+  btnFrances(){
+    this.idioma = 'fr';
+  }
+
 }
